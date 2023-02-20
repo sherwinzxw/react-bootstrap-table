c0705a1ab0bb39e3b70dfddae872b080420fed09
Xinwei (Sherwin) Zhao <sherwinzxw1990@gmail.com>
2023-02-20 21:54:29 +1100

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Bootstrap Table</h1>
+      <table>
+        
+      </table>
+    </div>  
+  );
+}
+
+export default App;
